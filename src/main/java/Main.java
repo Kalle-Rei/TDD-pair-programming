8f18d7cca8f8ff5fe3c7eb030a2fdb39de62a0e5
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-28 09:42:46 +0200

    additional tests and refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,26 +7,16 @@ public class Main {
         double celsius = (fahrenheit-32)*5/9;
         return celsius;
     }
-    public static int celsiusOrFahrenheit(double temperature){
+
+    public static String celsiusOrFahrenheit(double temperature){
         if (temperature >= 0 && temperature <= 30){
-            return 0; //Celsius
+            return "Celsius";
         }
         else if (temperature > 30){
-            return 1;//Fahrenheit
+            return "Fahrenheit";
         }
         else{
-            return 2;//invalid input
-        }
-    }
-    public static String celsiusOrFahrenheit2(double temperature){
-        if (temperature >= 0 && temperature <= 30){
-            return "Celsius"; //Celsius
-        }
-        else if (temperature > 30){
-            return "Fahrenheit";//Fahrenheit
-        }
-        else{
-            return "invalid input";//invalid input
+            return "invalid input";
         }
     }
 }
